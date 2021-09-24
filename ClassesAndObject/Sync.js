eed15e0e16b8b2bef35d295bc3543c0a57f5ab87
bhavani125 <girinenibhavani12@gmil.com>
2021-09-24 10:44:34 +0530

    Added:some practice problems

---
+++ ClassesAndObject/Sync.js
@@ -0,0 +1,10 @@
+//synchronous js (in js code will excute line by line)
+function myFunction(){
+    console.log("this block of function excutes when its called")
+}
+console.log("start");  // output1st start
+
+//calling the function here
+myFunction();           //output2nd (this block)
+
+console.log("finished")  //output3rd finished
